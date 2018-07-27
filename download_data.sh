7bb93b1768e9ec5ea92c0653f08dcbe7c574befe
JunaidCS032 <ansariahmedjunaid@gmail.com>
2018-07-27 17:56:25 +0530

    Updated the download script which downloads updated Data

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 ./third_party/gdown.pl https://drive.google.com/open?id=1X7oREQk4qE8z3po3vACZ4FKxPcWJF43A
-tar -zxvf gdown.tar.gz
+cd data
+tar -zxvf calib.tar.gz
+tar -zxvf Features2D_mat.tar.gz
+tar -zxvf images.tar.gz
+tar -zxvf ORBSLAM_pose.tar.gz
+tar -zxvf RRC_Detections_mat.tar.gz
+tar -zxvf RRC_Detections_txt.tar.gz
+
+rm calib.tar.gz
+rm Features2D_mat.tar.gz
+rm images.tar.gz
+rm ORBSLAM_pose.tar.gz
+rm RRC_Detections_mat.tar.gz
+rm RRC_Detections_txt.tar.gz
+
+
